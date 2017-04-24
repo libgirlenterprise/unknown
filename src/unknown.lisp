44b42a8b059a275b1f9435885c4a86ef1559a291
danielviolin <danielviolin0723@gmail.com>
2017-04-24 08:44:18 +0800

    half way get next unknown

--- src/unknown.lisp
+++ src/unknown.lisp
@@ -9,47 +9,65 @@
 
 (defvar *unknown-generator-app*)
 
-;; (smcl-thread-run '((:x nil
-;; 		    (:0
-;; 		     :0)
-;; 		    (:list-quote
-;; 		     (:car (:y :a :b)
-;; 		      :y)
-;; 		     :c))
-;; 		   (:y (:p1 :p2)
-;; 		    (:yp
-;; 		     :yp)
-;; 		    (:when :p1 :p2))
-;; 		   (:a (:p1 :p2)
-;; 		    (:a1
-;; 		     :0)
-;; 		    (:list-quote :true (:y :d)))
-;; 		   (:c nil
-;; 		    (:0
-;; 		     :0)
-;; 		    :e)
-;; 		   (:e (:p1)
-;; 		    (:0
-;; 		     :e2)
-;; 		    (:p1 (:list-quote :p1 :none)))
-;; 		   (:f (:x)
-;; 		    (:f1
-;; 		     (:list-quote (:defun :f :g)))
-;; 		    (:e (:y :x)))
-;; 		   (:8 (:p1)
-;; 		    (:3
-;; 		     :9)
-;; 		    (:when (:when :w) (:cdr :p1)))
-;; 		   (:z (:p1 :p2)
-;; 		    ((:list-quote :defun :8)
-;; 		     (:defun :f (:list-quote (:list-quote :3 (:list-quote :car :cdr)))))
-;; 		    (:eq :p1 (:p2 (:e :p1) :1)))))
+
 
 (setf *unknown-generator-app*
       (make-instance 'ningle:<app>))
 
 (setf (ningle:route *unknown-generator-app* "/")
-      "Welcome to unknown!")
+      (format nil
+	      "~a"
+	      (smcl-get-char)))
+
+(setf (ningle:route *unknown-generator-app* "/step/:count")
+      #'(lambda (params)
+	  (let ((step-count (parse-integer (cdr (assoc :count params))
+					   :junk-allowed t)))
+	    (and step-count
+		 (> step-count 0)
+		 (format nil
+			 "~a"
+			 (progn
+			   (smcl-run-steps :step-count step-count)
+			   step-count))))))
+
+;; (setf (ningle:route *unknown-generator-app* "/next")
+;;       "Welcome to unknown!")
 
 (defun unknown ()
+  (smcl-thread-run '((:x nil
+		      (:0
+		       :0)
+		      (:list-quote
+		       (:car (:y :a :b)
+			     :y)
+		       :c))
+		     (:y (:p1 :p2)
+		      (:yp
+		       :yp)
+		      (:when :p1 :p2))
+		     (:a (:p1 :p2)
+		      (:a1
+		       :0)
+		      (:list-quote :true (:y :d)))
+		     (:c nil
+		      (:0
+		       :0)
+		      :e)
+		     (:e (:p1)
+		      (:0
+		       :e2)
+		      (:p1 (:list-quote :p1 :none)))
+		     (:f (:x)
+		      (:f1
+		       (:list-quote (:defun :f :g)))
+		      (:e (:y :x)))
+		     (:8 (:p1)
+		      (:3
+		       :9)
+		      (:when (:when :w) (:cdr :p1)))
+		     (:z (:p1 :p2)
+		      ((:list-quote :defun :8)
+		       (:defun :f (:list-quote (:list-quote :3 (:list-quote :car :cdr)))))
+		      (:eq :p1 (:p2 (:e :p1) :1)))))
   (clack:clackup *unknown-generator-app*))
